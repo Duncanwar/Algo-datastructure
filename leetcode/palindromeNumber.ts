91c8069efdde10624e7bb757a9fb99e6f980be3a
Ndanyuzwe Duncan <semunda9@gmail.com>
2024-03-12 23:56:40 +0200

    palindrome

--- leetcode/palindromeNumber.ts
+++ leetcode/palindromeNumber.ts
@@ -1,7 +1,10 @@
 function isPalindrome(x: number): boolean {
     let num =x;
-    let rem=0;
-    while(num){
-        
+    let rem =0;
+    while(num>0){
+    rem = num%10
+    num = Math.trunc(num/10)
+
     }
+    return false
 };
