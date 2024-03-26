434571cfef397c56004479d03a9be830618c5eb9
Ndanyuzwe Duncan <semunda9@gmail.com>
2024-03-26 11:36:32 +0200

    Quick sort revised

--- 120Days/Day1-10:Sort/QuickSort.js
+++ 120Days/Day1-10:Sort/QuickSort.js
@@ -2,9 +2,25 @@ function quickSort(arr) {
     if(arr.length<=1){
         return arr
     }
-  let pivot = arr[high];
+  // Select a pivot element (for simplicity we choose the last element)
+  const pivot = arr[arr.length -1];
+  console.log(pivot)
+  const leftArr =[]
+  const rightArr = []
 
-  let i = low - 1;
+  // Partition the array into two subarrays
+  for (let i=0; i<arr.length -1; i++) {
+    if(arr[i]<pivot){
+      leftArr.push(arr[i]);
+    } else {
+      rightArr.push(arr[i]);
+    }
+  }
 
-  for (let j = low; j <= high; j++) {}
+  // Recursively sort the subarrays and concatenate them with the pivot
+  return [...quickSort(leftArr), pivot, ...quickSort(rightArr)];
 }
+
+const array = [12,11,13,5,6,7,14]
+const sortedArray = quickSort(array)
+console.log("Sorted array: " + sortedArray)
