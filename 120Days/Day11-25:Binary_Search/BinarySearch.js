83d4289d549786e069828a725311c464123eeab0
Ndanyuzwe Duncan <semunda9@gmail.com>
2024-04-02 23:57:16 +0200

    day 11 binary search

--- 120Days/Day11-25:Binary_Search/BinarySearch.js
+++ 120Days/Day11-25:Binary_Search/BinarySearch.js
@@ -1,3 +1,21 @@
-function binarySearch(int r) {
+function binarySearch(arr, x) {
+  let left = 0;
+  let right = arr.length - 1;
+  let middle;
 
+  while (left <= right) {
+    middle = (left + (right - left)) >> 1;
+    if (arr[middle] == x) {
+      return middle;
+    }
+    if (arr[left] < x) {
+      middle= left - 1;
+    }
+    else 
+    middle= right -1
+  }
+  return -1
 }
+
+const result = binarySearch([1, 4, 7, 9, 10, 11], 1);
+console.log(result);
