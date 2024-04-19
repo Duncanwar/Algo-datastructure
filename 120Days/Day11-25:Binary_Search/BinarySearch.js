22717ce73088611ed59b7f701981612641e6b52c
Duncanwar <semunda9@gmail.com>
2024-04-19 10:05:31 +0200

    changed

--- 120Days/Day11-25:Binary_Search/BinarySearch.js
+++ 120Days/Day11-25:Binary_Search/BinarySearch.js
@@ -1,21 +1,21 @@
 function binarySearch(arr, x) {
-  let left = 0;
-  let right = arr.length - 1;
-  let middle;
+    let left = 0;
+    let right = arr.length - 1;
+    let middle;
 
-  while (left <= right) {
-    middle = left + ((right - left) >> 1);
-    if (arr[middle] === x) {
-      return middle;
+    while (left <= right) {
+        middle = left + ((right - left) >> 1);
+        if (arr[middle] === x) {
+            return middle;
+        }
+        if (arr[middle] < x) {
+            left = middle + 1;
+        } else {
+            right = middle - 1;
+        }
     }
-    if (arr[middle] < x) {
-      left= middle + 1;
-    }
-    else 
-    right= middle -1
-  }
-  return -1
+    return -1;
 }
 
-const result = binarySearch([1, 4, 7, 9, 10, 11], 1);
+const result = binarySearch([1, 4, 7, 9, 10, 11], 7);
 console.log(result);
